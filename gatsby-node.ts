b6a1f996251525a6c0418e1fa081b956e01c4523
unknown <termissues@gmail.com>
2024-11-13 19:27:42 +0200

    Initial commit

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,25 +1,40 @@
-exports.createPages = async ({ actions, graphql }: { actions: any, graphql: any }) => {
+import { GatsbyNode } from "gatsby";
+import path from "path";
 
-    const { createPage } = actions;
-    const result = await graphql(`
-      {
-        allDataJson {
-          edges {
-            node {
-              name
-            }
+export const createPages: GatsbyNode["createPages"] = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const result = await graphql<{
+    allDataJson: {
+      edges: {
+        node: {
+          name: string;
+        };
+      }[];
+    };
+  }>(`
+    {
+      allDataJson {
+        edges {
+          node {
+            name
           }
         }
       }
-    `);
+    }
+  `);
 
-    result.data.allDataJson.edges.forEach(({ node }: { node: { name: string } }) => {
-        createPage({
-            path: `/about/${node.name.toLowerCase()}`,
-            component: require.resolve("./src/templates/about.tsx"),
-            context: {
-                name: node.name,
-            },
-        });
+  if (result.errors) {
+    throw new Error("Failed to fetch data for page creation.");
+  }
+
+  result.data?.allDataJson.edges.forEach(({ node }: { node: { name: string } }) => {
+    createPage({
+      path: `/about/${node.name.toLowerCase()}`,
+      component: path.resolve("./src/templates/about.tsx"),
+      context: {
+        name: node.name,
+      },
     });
+  });
 };
